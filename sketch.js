241bc112f312f32dfcbb5778059b7fe729437383
shawnseb <72581921+shawnseb@users.noreply.github.com>
2020-10-19 21:21:49 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,34 @@
-function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
-}
+var car;
+var wall;
+var speed;
+var weight;
 
+  function setup (){
+    car=createSprite(50,200,50,50);
+wall=createSprite(300,200,50,200);
+speed=random(55,90);
+  weight=random(400,1500);
+  }
 function draw() {
-  background(255,255,255);  
+   background("blue");
+  
+  car.velocityX=speed;
+if(0.5*weight*speed*speed/22500<=100 && car.isTouching(wall)){
+ car.shapeColor="green";
+ 
+}
+if(0.5*weight*speed*speed/22500>=180 && car.isTouching(wall)){
+  car.shapeColor="red";
+  
+}
+if(0.5*weight*speed*speed/22500>=100 && 0.5*weight*speed*speed/22500 < 180 && car.isTouching(wall)){
+  car.shapeColor="yellow";
+}
+if(car.isTouching(wall)){
+  car.velocityX=0;
+}
+textFont("white");
+text(speed, 50,200);
+text(weight,70,220);
   drawSprites();
 }
